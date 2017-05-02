22eb60caf8481c2dd5b9fccbd3bc7c389d3744e7
Johanna D <johanna.dietzz@gmail.com>
2017-05-02 18:16:36 +0200

    added edit buttons in /edit-task page

--- client/main.js
+++ client/main.js
@@ -3,5 +3,5 @@ import '../imports/client/ui/components/task';
 
 import '../imports/client/ui/pages/create_task';
 import '../imports/client/ui/pages/edit_task';
-import '../imports/client/ui/pages/list.js';
+import '../imports/client/ui/pages/list';
 import '../imports/client/ui/pages/impressum';
